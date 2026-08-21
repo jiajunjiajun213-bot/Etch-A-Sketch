afce97a78ac733a44cfba5207c282b425517fb6d
Felicia <jiajunjiajun213@gmail.com>
2026-08-21 17:09:44 +0800

    Added square divs using for loop and also eventlistener so it chnages color when hover over

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-# Etch-A-Sketch
-# Etch-A-Sketch
+ const container = document.querySelector(".container")
+ let gridSize = prompt("what is your griz size from 1 to 100")
+ if(!(Number.isInteger(gridSize) && !(gridSize > 0 && gridSize <= 100 ))){
+    alert ("ERROR")
+ }
+
+ for (let i = 0 ; i < 16 ; i++){
+   for (let i = 0 ; i <16 ; i++){
+    const div = document.createElement('div')
+    container.appendChild(div);
+    div.className = "squareDiv"
+    div.addEventListener("mouseenter" ,  e => {
+    e.target.style.backgroundColor = 'black'
+ })
+   }
+ }
+
