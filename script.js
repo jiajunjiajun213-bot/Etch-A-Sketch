cd3400675ebdbced98500185305d0b92d80bd7e2
Felicia <jiajunjiajun213@gmail.com>
2026-08-24 01:11:56 +0800

    managed to implement the grid size feature to change size , managed to remove each container after changing a new size , managed to choose size when click on the button and also put all of them together except the start where it should be 16x16

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
- const container = document.querySelector(".container")
- let gridSize = prompt("what is your griz size from 1 to 100")
- if(!(Number.isInteger(gridSize) && !(gridSize > 0 && gridSize <= 100 ))){
-    alert ("ERROR")
- }
+const container = document.querySelector(".container")
+const button = document.querySelector(".button")
 
- for (let i = 0 ; i < 16 ; i++){
-   for (let i = 0 ; i <16 ; i++){
+ function gridSize(){
+     let gridSizeOne = prompt ("Choose number from 1 to 100 ?" )
+     let gridSizeTwo = Number(gridSizeOne)
+     if (!(Number.isInteger (Number(gridSizeTwo))) || (Number(gridSizeTwo) < 1 || Number(gridSizeTwo) > 100)){
+     return alert("Error");
+ }
+     else{
+     return gridSizeTwo;
+    }   
+    }
+
+button.addEventListener('click' , function(){
+
+   container.innerHTML = ""
+   let result = gridSize();
+   for (let i = 0 ; i < result ; i++){
+   for (let j = 0 ; j <result ; j++){
     const div = document.createElement('div')
     container.appendChild(div);
     div.className = "squareDiv"
+    div.style.flex =`1 1 calc(100% / ${result})`
     div.addEventListener("mouseenter" ,  e => {
     e.target.style.backgroundColor = 'black'
  })
    }
  }
 
+})
+
+ 
